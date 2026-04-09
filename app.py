d8181e244d55e5638fd29a01b80fa20d1c6aeca2
Hiroe-art <hfukunaga9@gmail.com>
2026-04-09 14:29:49 +0900

    Refactor app.py for improved code clarity

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license" for more information.
 ...     st.info(q_data["問題文"])
 ...
